6f85978e471d61dd8eb11726b0ece5e0c632dd06
godkate <bogodul883@gmail.com>
2022-12-29 13:53:38 -0500

    Redo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const arr = [1, 2, 3, 4, 5, 6, 7];
 function removeElement (array, item) {
-        array.splice(array.indexOf(item), 1);
-        return array;
+        return array.filter((elem) => {
+                if (elem !== item) {
+                        return elem;
+                }
+        });
 };
 console.log(removeElement(arr, 5));
